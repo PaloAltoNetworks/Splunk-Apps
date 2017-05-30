3fe14703127f3e2f1195b7a9b18503902c84dfce
Paul Nguyen <panguyen@paloaltonetworks.com>
2017-05-30 14:41:50 -0700

    TA check completed

--- appserver/static/dashboard.js
+++ appserver/static/dashboard.js
@@ -1,74 +1,94 @@
-require.config({
-  baseUrl: "{{SPLUNKWEB_URL_PREFIX}}/static/js",
-  // waitSeconds: 0 // Disable require.js load timeout
-});
-
-  var splunk_libraries = [
-    "jquery"
-  ]
-  var splunk_function = function (
+require([
+  'splunkjs/ready!',
+  'splunkjs/mvc/simplexml/ready!',
+  'underscore',
+  "jquery"
+  ], function(
+    mvc,
+    ignored,
+    _,
     $
-    ){ 
-      console.log("in dashboard.js");
-      // sessionKey = settings.get("sessionKey", None);
-      var ignore_TA = localStorage.ignore_ta
-      console.log(ignore_TA);
+  ) {
+    // Required TA version
+    var dependencyVersion = "3.8.0";
 
-      if(!ignore_TA) {
-        console.log("They didn't want to ignore me so lets check if they seen the modal this session.");
-        var check_TA = sessionStorage.checked_ta
-      } else {
-        console.log("looks like they want to ignore me");
-        var check_TA = 1;
-      }
-      console.log(check_TA);
+    // This is to see if they have asked to ignore this check.
+    var ignore_TA = localStorage.ignore_ta
+    // console.log(ignore_TA);
+    if(!ignore_TA) {
+      // console.log("They didn't want to ignore me so lets check if they seen the modal this session.");
+      var check_TA = sessionStorage.checked_ta
+    } else {
+      // console.log("looks like they want to ignore me");
+      var check_TA = 1;
+    }
 
-      if (!check_TA) {
-        console.log("inside ta_check");
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "https://localhost:8089/services/apps/local/Splunk_TA_paloalto", true);
-        xhttp.send();
-        console.log(xhttp.responseText);
+    // Only check if the TA is install once per a session.
+    if (!check_TA) {
+      var service = mvc.createService();
+      service.apps()
+      .fetch(function(err, apps) {
+        if (err) {
+          sendDevLog( err);
+          console.error(err);
+          return;
+        }
 
-        // $.ajax({
-        //   url: 'http://localhost:8089/services/apps/local',
-        //   data: 'output_mode=json',
-        //   dataType: 'json',
-        //   success: function(data){
-        //     console.log('success',data);
-        //   }
-        // });
-        // sessionStorage.checked_ta = 1;
-
-        var htmlData = '<div class="modal fade" role="dialog" tabindex="-1" id="pan_message"> \
-        <div class="modal-dialog" role="document"> \
-          <div class="modal-content"> \
-            <div class="modal-header"> \
-              <h4 class="modal-title">  <span class="glyphicon glyphicon-star" aria-hidden="true"></span> Missing Add-on. </h4> \
-            </div> \
-            <div class="modal-body"> \
-              <p>Please install the <a href="https://splunkbase.splunk.com/app/2757/" target="_blank">Palo Alto Networks Add-on</a>.</p> \
-              <!-- <input type="checkbox" id="ignore_ta_check" /> I understand the TA is not installed. Don\'t show me this again. --> \
-            </div> \
-            <div class="modal-footer"> \
-              <button type="button" class="btn btn-default" data-dismiss="modal" id="ta_check_close">Close</button> \
-            </div> \
-          </div> \
-        </div>';
-        
-        $( "body" ).append(htmlData);
-        $( "#pan_message" ).modal('show');
-      }
-
-      $( "#ta_check_close" ).click(function() {
-        ignore_ta_check = $('#ignore_ta_check').is(":checked");
-        if(ignore_ta_check === true) {
-          console.log("yes its checked");
-          localStorage.ignore_ta = 1;
-        } 
+        // Check if the Add-on is installed.
+        var paloaltoTA = apps.item('Splunk_TA_paloalto');
+        if (!paloaltoTA) {
+          // TA is not installed.
+          var title = "Missing Add-on";
+          var message = '<p>Please install the <a href="https://splunkbase.splunk.com/app/2757/" target="_blank">Palo Alto Networks Add-on</a>.</p> \
+            <p>For more information please view the <a href="http://pansplunk.readthedocs.io/en/latest/" target="_blank">getting started documentation</a>.</p>';
+          deployModal(title, message);
+          sessionStorage.checked_ta = 1;
+        } else {
+          // TA is installed
+          // Check the version to see if it matches dependency.
+          var looseVersion =  paloaltoTA._properties.version;
+          if (looseVersion >= dependencyVersion) {
+            // Everything passed. Adding to session storage to not check the TA anymore.
+            sessionStorage.checked_ta = 1;
+          } else {
+            var title = "Add-on Dependency Warning";
+            var message = '<p>The current verison of the Palo Alto Networks Add-on you have installed does not match the required version needed for this app. </p>\
+              <p>Installed version: ' + looseVersion + '</p> \
+              <p>The version required is: ' + dependencyVersion + '</p> \
+              <p>Please download the required version from <a href="https://splunkbase.splunk.com/app/2757/" target="_blank">Splunk Base</a></p>';
+            deployModal(title, message); 
+            sessionStorage.checked_ta = 1;
+          }
+        }
       });
+    }
+    // FUTURE USE if we have a checkbox for never checking TA again.
+    // $( "#ta_check_close" ).click(function() {
+    //   ignore_ta_check = $('#ignore_ta_check').is(":checked");
+    //   if(ignore_ta_check === true) {
+    //     console.log("yes its checked");
+    //     localStorage.ignore_ta = 1;
+    //   } 
+    // });
+});
+// Function to deploy the modal.
+function deployModal(title, message) {
+  console.log("modal deployed");
+  var htmlData = '<div class="modal fade" role="dialog" tabindex="-1" id="pan_message"> \
+    <div class="modal-dialog" role="document"> \
+    <div class="modal-content"> \
+    <div class="modal-header"> \
+    <h4 class="modal-title">' + title + '</h4> \
+    </div> \
+    <div class="modal-body"> \
+    ' + message + ' \
+    </div> \
+    <div class="modal-footer"> \
+    <button type="button" class="btn btn-default" data-dismiss="modal" id="ta_check_close">Close</button> \
+    </div> \
+    </div> \
+    </div>';
 
-      console.log("done dashboard.js");
-    }; //end splunk function
-//return our renderer
-require(splunk_libraries, splunk_function);
+    $( "body" ).append(htmlData);
+    $( "#pan_message" ).modal('show');
+}
