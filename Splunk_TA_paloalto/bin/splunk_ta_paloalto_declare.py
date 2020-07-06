c52033aa30e2620ca71477071e86eda94210c2b7
Paul Nguyen <panguyen@paloaltonetworks.com>
2020-07-06 12:55:52 -0700

    chore(addon): Update Add-on Builder libs to 3.0.1
    Updates Add-on Builder libraries that come with the add-on
    from Addon Builder 2.2.0 to 3.0.1.
    This only changes files owned by Splunk Add-on Builder.
    No Palo Alto Networks files are affected by this commit.

--- Splunk_TA_paloalto/bin/splunk_ta_paloalto_declare.py
+++ Splunk_TA_paloalto/bin/splunk_ta_paloalto_declare.py
@@ -9,9 +9,15 @@ import os
 import sys
 import re
 
+if sys.version_info[0] < 3:
+    py_version = "aob_py2"
+else:
+    py_version = "aob_py3"
+
 ta_name = 'Splunk_TA_paloalto'
 ta_lib_name = 'splunk_ta_paloalto'
 pattern = re.compile(r"[\\/]etc[\\/]apps[\\/][^\\/]+[\\/]bin[\\/]?$")
 new_paths = [path for path in sys.path if not pattern.search(path) or ta_name in path]
 new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name]))
+new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name, py_version]))
 sys.path = new_paths
