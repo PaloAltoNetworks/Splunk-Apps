c12fda41ed7a4d6b87870cf26b8b4d36012bb50a
Paul Nguyen <panguyen@paloaltonetworks.com>
2021-09-21 15:49:25 -0700

    Fix custom Javasript error and remove TA dependency check
    Closes #216

--- SplunkforPaloAltoNetworks/appserver/static/tooltip.js
+++ SplunkforPaloAltoNetworks/appserver/static/tooltip.js
@@ -1,3 +1,22 @@
+function deployModal(title, message) {
+  var htmlData = '<div class="modal fade" role="dialog" tabindex="-1" id="pan_message"> \
+  <div class="modal-dialog" role="document"> \
+  <div class="modal-content"> \
+  <div class="modal-header"> \
+  <h3 class="modal-title">' + title + '</h3> \
+  </div> \
+  <div class="modal-body"> \
+  ' + message + ' \
+  </div> \
+  <div class="modal-footer"> \
+  <button type="button" class="btn btn-default" data-dismiss="modal" id="ta_check_close">Close</button> \
+  </div> \
+  </div> \
+  </div>';
+
+  $('body').append(htmlData);
+  $('#pan_message').modal('show');
+}
 require([
   'underscore',
   'jquery',
