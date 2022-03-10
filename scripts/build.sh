a2712ee770393c84bccf95b9a647b256114f557f
Brian Torres-Gil <btorres-gil@paloaltonetworks.com>
2022-03-09 16:05:51 -0800

    build: Build script can set version during build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ ROOT=${SCRIPT_BASE}/..
 source "$SCRIPT_BASE/log4bash.sh"
 # shellcheck source=/dev/null
 source "$SCRIPT_BASE/common.sh"
+log_debug "DEBUG ENABLED"
 
 # Exit immediatly if any command exits with a non-zero status
 set -e
@@ -16,19 +17,22 @@ print_usage () {
     echo ""
     echo "Usage:"
     echo ""
-    echo "build.sh -a <app or addon> [-o filename] [-l]"
+    echo "build.sh -a <app or addon> [-o filename] [-l] [-v <version> [-r <release-channel>]]"
     echo ""
     echo "  -a What to build. Must be either 'app' or 'addon'."
     echo "  -o The filename of the output package"
     echo "  -l Build from local files, not from git-archive"
+    echo "  -v Set the version before building"
+    echo "  -r When setting the version, specify a release channel (see set-version.sh)"
     echo ""
 }
 
 APP='Splunk_TA_paloalto'
 
-while getopts a:o:lh FLAG; do
+while getopts a:o:v:r:lh FLAG; do
   case $FLAG in
     a)
+      WHICHAPP=$OPTARG
       if [ "$OPTARG" == "app" ]; then
         APP=SplunkforPaloAltoNetworks
       elif [ "$OPTARG" == "addon" ]; then
@@ -41,12 +45,22 @@ while getopts a:o:lh FLAG; do
     o)
       OUTPUT_FILE="$OPTARG"
       ;;
+    v)
+      SET_VERSION="$OPTARG"
+      ;;
+    r)
+      CHANNEL="$OPTARG"
+      ;;
     l)
       BUILD_LOCAL="true"
       ;;
     h)
       print_usage
       exit 0
+      ;;
+    :)
+      echo "$0: Must supply an argument to -$OPTARG." >&2
+      exit 1
       ;;
     \?) #unrecognized option - show help
       print_usage
@@ -60,9 +74,16 @@ rm -rf "${ROOT}/_build/tmp" 2>/dev/null
 mkdir -p "${ROOT}/_build/tmp"
 
 # Get the current version from the app
-VERSION=$(get_version "$ROOT/$APP")
+if [ "$SET_VERSION" ]; then
+    log_debug "Using specified version: $SET_VERSION"
+    VERSION="$SET_VERSION"
+else
+    log_debug "Using version from app.conf"
+    VERSION=$(get_version "$ROOT/$APP")
+fi
 
 if [ -z "$BUILD_LOCAL" ]; then
+    log_debug "Building from git archive"
     BRANCH=$(get_branch)
     BUILD=$(get_build)
 
@@ -77,25 +98,36 @@ if [ -z "$BUILD_LOCAL" ]; then
     tar xzf "${APP}-${VERSION}-${BUILD}-temp.tgz"
     rm "${APP}-${VERSION}-${BUILD}-temp.tgz"
 else
-    BRANCH=no-branch
+    log_debug "Building from local"
+    BRANCH=$(get_branch)
     BUILD=local
 
     log_info "Building ${APP} version ${VERSION} build ${BUILD} from branch ${BRANCH}"
 
     # Just copy the files, don't use git. Safe during CI, but not anywhere else.
-    cp -R "${ROOT}/${APP}" "${ROOT}/_build/tmp/"
     cd "${ROOT}/_build/tmp"
+    rm -rf "${APP}" >/dev/null
+    cp -R "${ROOT}/${APP}" "${ROOT}/_build/tmp/"
 fi
 
 # Strip out stuff that SplunkBase doesn't like
 # such as hidden files and Makefiles
-find . -type f -name ".*" -exec rm {} \;
+find ${APP} -type f -name ".*" -exec rm {} \;
+find ${APP} -type d -name ".*" -exec rm -rf {} \;
+find ${APP} -type f -name "*.pyc" -exec rm {} \;
 find "${APP}/bin" -type f -name "*.py" -exec chmod +x {} \;
 rm "${APP}/Makefile"
 rm "${APP}/bin/lib/pandevice/docs/Makefile"
 rm "${APP}/bin/lib/pandevice/docs/make.bat"
 rm "${APP}/bin/lib/pan-python/doc/Makefile"
 rm -rf "${APP}/release"
+rm -rf "${APP}/local"
+
+# Set the version if requested
+if [ "$SET_VERSION" ]; then
+    log_debug "Running set-version.sh..."
+    LOG_LEVEL=$LOG_LEVEL "$SCRIPT_BASE/set-version.sh" -a "$WHICHAPP" -d ./ "$SET_VERSION" "$CHANNEL"
+fi
 
 if [ -z "$OUTPUT_FILE" ]; then
     FILENAME=$(get_build_filename "$APP" "$VERSION" "$BRANCH" "$BUILD")
@@ -108,12 +140,16 @@ tar czf "${ROOT}/_build/${FILENAME}" "${APP}"
 
 # Set variables for GitHub Actions to pick up in subsequent steps
 if [ "$GITHUB_ACTIONS" == "true" ]; then
-    echo ::set-output "name=path::_build/${FILENAME}"
-    echo ::set-output "name=file::${FILENAME}"
+    log_debug "Setting GitHub Actions outputs"
+    echo ::set-output "name=path::_build/$FILENAME"
+    echo ::set-output "name=file::$FILENAME"
 fi
+
+log_debug "path: _build/$FILENAME"
+log_debug "file: $FILENAME"
 
 # Cleanup
 cd "${ROOT}"
 rm -rf "${ROOT}/_build/tmp"
 
-log_success "SplunkBase package is ready."
+log_success "SplunkBase package is ready at _build/$FILENAME"
--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -16,6 +16,12 @@ README=README.md
 APPMANIFEST=app.manifest
 GLOBALCONFIG=appserver/static/js/build/globalConfig.json
 
+# Set default working directory
+WORK=$ROOT
+
+# Set which app to set the version of
+WHICHAPP=both
+
 # Exit immediatly if any command exits with a non-zero status
 set -e
 
@@ -24,23 +30,53 @@ print_usage() {
     echo "Set the app/add-on version"
     echo ""
     echo "Usage:"
-    echo "  set-version.sh <new-version> [release-channel]"
+    echo "  $(basename "$0") [-d <root-directory>] <new-version> [release-channel]"
     echo ""
     echo "Release channel can be 'default', 'beta', or 'alpha'."
     echo "If not specified, the default channel is used."
 }
 
-# if less than one arguments supplied, display usage
+# Process arguments
+while getopts a:d:h FLAG; do
+  case $FLAG in
+    a)
+      if [ "$OPTARG" == "app" ]; then
+        WHICHAPP=app
+      elif [ "$OPTARG" == "addon" ]; then
+        WHICHAPP=addon
+      elif [ "$OPTARG" == "both" ]; then
+        WHICHAPP=both
+      else
+        log_error "Unknown argument: $OPTARG"
+        exit 1
+      fi
+      ;;
+    d)
+      WORK="$OPTARG"
+      ;;
+    h)
+      print_usage
+      exit 0
+      ;;
+    :)
+      echo "$0: Must supply an argument to -$OPTARG." >&2
+      exit 1
+      ;;
+    \?) #unrecognized option - show help
+      print_usage
+      exit 1
+      ;;
+  esac
+done
+
+# Remove getopts arguments
+shift "$(($OPTIND -1))"
+
+# If less than one arguments supplied, display usage
 if [  $# -lt 1 ]
 then
     print_usage
     exit 1
-fi
-
-# check whether user had supplied -h or --help . If yes display usage
-if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-    print_usage
-    exit 0
 fi
 
 TEMP_VERSION=${1/-alpha\./alpha}
@@ -48,7 +84,7 @@ NEW_VERSION=${TEMP_VERSION/-beta\./beta}
 CHANNEL=${2:-default}
 
 # Get the current version from the app
-CURRENT_VERSION=$(grep -o '^version = [0-9a-z.-]*' "$ROOT/$APP/default/app.conf" | awk '{print $3}')
+CURRENT_VERSION=$(grep -o '^version = [0-9a-z.-]*' "$WORK/$APP/default/app.conf" | awk '{print $3}')
 # Generate a build number
 if [ "$TRAVIS" == "true" ]; then
     log_debug "Running in TravisCI"
@@ -86,71 +122,75 @@ log_info "Changing version from $CURRENT_VERSION to $NEW_VERSION build $BUILD on
 # exists in the file. If grep fails to find the line, the whole script stops
 # with a return code of 1. After each grep, sed does the replacement.
 
-# Set App versions
-FILE="${ROOT}/${APP}/${APPCONF}"
+if [ "$WHICHAPP" = "app" ] || [ "$WHICHAPP" = "both" ]; then
+  # Set App versions
+  FILE="${WORK}/${APP}/${APPCONF}"
 
-log_debug "Set App ${APPCONF} version to ${NEW_VERSION}"
-grep -E '^version = .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/version = .+/version = ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set App ${APPCONF} version to ${NEW_VERSION}"
+  grep -E '^version = .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/version = .+/version = ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
 
-log_debug "Set App ${APPCONF} build to ${BUILD}"
-grep -E '^build = .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/build = .+/build = ${BUILD}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set App ${APPCONF} build to ${BUILD}"
+  grep -E '^build = .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/build = .+/build = ${BUILD}/" "$FILE" && rm "${FILE}.bak"
 
-log_debug "Set App ${APPCONF} add-on required version to ${NEW_VERSION}"
-grep -E '^ta_dependency_version = .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/ta_dependency_version = .+/ta_dependency_version = ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set App ${APPCONF} add-on required version to ${NEW_VERSION}"
+  grep -E '^ta_dependency_version = .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/ta_dependency_version = .+/ta_dependency_version = ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
 
-FILE="${ROOT}/${APP}/${README}"
+  FILE="${WORK}/${APP}/${README}"
 
-log_debug "Set App ${README} version to ${NEW_VERSION}"
-grep -E 'App Version:\*\* .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/App Version:\*\* .+/App Version:** ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set App ${README} version to ${NEW_VERSION}"
+  grep -E 'App Version:\*\* .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/App Version:\*\* .+/App Version:** ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
 
-log_debug "Set App ${APP}/${README} add-on required version to ${NEW_VERSION}"
-grep -E 'Splunk_TA_paloalto .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/Splunk_TA_paloalto .+/Splunk_TA_paloalto ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set App ${APP}/${README} add-on required version to ${NEW_VERSION}"
+  grep -E 'Splunk_TA_paloalto .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/Splunk_TA_paloalto .+/Splunk_TA_paloalto ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
 
-FILE="${ROOT}/${APP}/${APPMANIFEST}"
+  FILE="${WORK}/${APP}/${APPMANIFEST}"
 
-log_debug "Set App ${APPMANIFEST} version to ${NEW_VERSION}"
-grep -E '\"version\": .+' "$FILE" >/dev/null
-sed -i.bak -E "s/version\": .+/version\": \"${NEW_VERSION}\"/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set App ${APPMANIFEST} version to ${NEW_VERSION}"
+  grep -E '\"version\": .+' "$FILE" >/dev/null
+  sed -i.bak -E "s/version\": .+/version\": \"${NEW_VERSION}\"/" "$FILE" && rm "${FILE}.bak"
 
-log_debug "Set Addon ${APPMANIFEST} development status to ${DEVSTATUS}"
-grep -E '\"developmentStatus\": .+' "$FILE" >/dev/null
-sed -i.bak -E "s/developmentStatus\": .+/developmentStatus\": \"${DEVSTATUS}\"/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set Addon ${APPMANIFEST} development status to ${DEVSTATUS}"
+  grep -E '\"developmentStatus\": .+' "$FILE" >/dev/null
+  sed -i.bak -E "s/developmentStatus\": .+/developmentStatus\": \"${DEVSTATUS}\"/" "$FILE" && rm "${FILE}.bak"
+fi
 
-# Set Add-on versions
+if [ "$WHICHAPP" = "addon" ] || [ "$WHICHAPP" = "both" ]; then
+  # Set Add-on versions
 
-FILE="${ROOT}/${ADDON}/${APPCONF}"
+  FILE="${WORK}/${ADDON}/${APPCONF}"
 
-log_debug "Set Addon ${APPCONF} version to ${NEW_VERSION}"
-grep -E '^version = .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/version = .+/version = ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set Addon ${APPCONF} version to ${NEW_VERSION}"
+  grep -E '^version = .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/version = .+/version = ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
 
-log_debug "Set Addon ${APPCONF} build to ${BUILD}"
-grep -E '^build = .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/build = .+/build = ${BUILD}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set Addon ${APPCONF} build to ${BUILD}"
+  grep -E '^build = .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/build = .+/build = ${BUILD}/" "$FILE" && rm "${FILE}.bak"
 
-FILE="${ROOT}/${ADDON}/${APPMANIFEST}"
+  FILE="${WORK}/${ADDON}/${APPMANIFEST}"
 
-log_debug "Set Addon ${APPMANIFEST} version to ${NEW_VERSION}"
-grep -E '\"version\": .+' "$FILE" >/dev/null
-sed -i.bak -E "s/version\": .+/version\": \"${NEW_VERSION}\"/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set Addon ${APPMANIFEST} version to ${NEW_VERSION}"
+  grep -E '\"version\": .+' "$FILE" >/dev/null
+  sed -i.bak -E "s/version\": .+/version\": \"${NEW_VERSION}\"/" "$FILE" && rm "${FILE}.bak"
 
-log_debug "Set Addon ${APPMANIFEST} development status to ${DEVSTATUS}"
-grep -E '\"developmentStatus\": .+' "$FILE" >/dev/null
-sed -i.bak -E "s/developmentStatus\": .+/developmentStatus\": \"${DEVSTATUS}\"/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set Addon ${APPMANIFEST} development status to ${DEVSTATUS}"
+  grep -E '\"developmentStatus\": .+' "$FILE" >/dev/null
+  sed -i.bak -E "s/developmentStatus\": .+/developmentStatus\": \"${DEVSTATUS}\"/" "$FILE" && rm "${FILE}.bak"
 
-FILE="${ROOT}/${ADDON}/${README}"
+  FILE="${WORK}/${ADDON}/${README}"
 
-log_debug "Set Addon ${README} Version to ${NEW_VERSION}"
-grep -E 'Add-on Version:\*\* .+$' "$FILE" >/dev/null
-sed -i.bak -E "s/Add-on Version:\*\* .+/Add-on Version:** ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set Addon ${README} Version to ${NEW_VERSION}"
+  grep -E 'Add-on Version:\*\* .+$' "$FILE" >/dev/null
+  sed -i.bak -E "s/Add-on Version:\*\* .+/Add-on Version:** ${NEW_VERSION}/" "$FILE" && rm "${FILE}.bak"
 
-FILE="${ROOT}/${ADDON}/${GLOBALCONFIG}"
+  FILE="${WORK}/${ADDON}/${GLOBALCONFIG}"
 
-log_debug "Set Addon GlobalConfig Version to ${NEW_VERSION}"
-grep -E '\"version\": .+' "$FILE" >/dev/null
-sed -i.bak -E "s/version\": .+/version\": \"${NEW_VERSION}\",/" "$FILE" && rm "${FILE}.bak"
+  log_debug "Set Addon GlobalConfig Version to ${NEW_VERSION}"
+  grep -E '\"version\": .+' "$FILE" >/dev/null
+  sed -i.bak -E "s/version\": .+/version\": \"${NEW_VERSION}\",/" "$FILE" && rm "${FILE}.bak"
+fi
